9b007e3d0a7d37edb35e397f1a5f412378a594dd
Sarwer Hussain <48062618+hussainint@users.noreply.github.com>
2019-09-29 14:28:13 +0530

    this is the source code of CUBE

---
+++ cube.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+public class area{
+     void main(){
+         Scanner sc=new Scanner(System.in);
+         System.out.println("\n\n\n\t\t          _____________________");
+         System.out.println("\t\t       / |                   /|");
+         System.out.println("\t\t     /   |                 /  |");
+         System.out.println("\t\t   /     |               /    |");
+         System.out.println("\t\t /       |             /      |");
+         System.out.println("\t\t ________|__________ /        |");
+         System.out.println("\t\t|        |          |         |");
+         System.out.println("\t\t|        |          |         |");
+         System.out.println("\t\t|        |          |         |");
+         System.out.println("\t\t|        |          |         |");
+         System.out.println("\t\t|        |__________|_________");
+         System.out.println("\t\t|      /            |        /");
+         System.out.println("\t\t|    /              |      /  ");
+         System.out.println("\t\t|  /                |    /    ");
+         System.out.println("\t\t|/                  |  /      ");
+         System.out.println("\t\t|___________________|/        ");
+        }
+    }
